893d4c0d6b22c79187e9a08231414ee4bb91846b
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-07-15 12:56:23 -0700

    minor changes

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    images: {
+      domains: [
+        'localhost', // For local development
+        'marie-sooty.vercel.app', // Your Vercel deployment
+      ],
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require('./scripts/generate-sitemap');
+      }
+      return config;
+    },
+  };
+  
