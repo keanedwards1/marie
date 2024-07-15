404946a6e64d35479fa3a543ee1af4215d936702
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-07-15 11:35:02 -0700

    removed go_backend in favor of node.js with typescript

--- src/components/WisteriaRow.tsx
+++ src/components/WisteriaRow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 interface WisteriaProps {
@@ -30,6 +30,12 @@ const WisteriaRow: React.FC = () => {
   const wisteriaCount = 30;
   const wisteriaWidth = 100;
   const spreadFactor = 1.4; // Adjust this value to increase the spread
+  const [offsetYValues, setOffsetYValues] = useState<number[]>([]);
+
+  useEffect(() => {
+    const randomValues = Array.from({ length: wisteriaCount }, () => Math.random() * 20);
+    setOffsetYValues(randomValues);
+  }, []);
 
   const getRandomOverlapFactor = () => {
     return 0.5 + Math.random() * 0.015; // Random value between 0.5 and 0.515
@@ -37,13 +43,14 @@ const WisteriaRow: React.FC = () => {
 
   return (
     <section className="relative w-full h-56 z-0 overflow-hidden">
-      {[...Array(wisteriaCount)].map((_, index) => {
+      {offsetYValues.length > 0 && [...Array(wisteriaCount)].map((_, index) => {
         const overlapFactor = getRandomOverlapFactor();
+        const offsetY = offsetYValues[index];
         return (
           <Wisteria 
             key={index} 
             offsetX={index * wisteriaWidth * overlapFactor * spreadFactor} 
-            offsetY={Math.random() * 20}
+            offsetY={offsetY}
             zIndex={index}
             delay={index * 0.5} // Staggered delay
           />
