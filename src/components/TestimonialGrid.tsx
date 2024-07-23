93ed862f2151cb73556e2e888c9b86b33a88ba0d
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-07-22 18:43:22 -0700

    got the admin panel to work, lots of small bug fixing along the way and small style changes

--- src/components/TestimonialGrid.tsx
+++ src/components/TestimonialGrid.tsx
@@ -1,160 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import Masonry from 'react-masonry-css';
+
+type Review = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  job_title: string;
+  review: string;
+  status: string;
+  created_at: number;
+};
 
 const TestimonialGrid: React.FC = () => {
+  const [reviews, setReviews] = useState<Review[]>([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await fetch('https://159.89.233.75.nip.io/api/reviews');
+        if (response.status === 429) {
+          console.log('Too many submissions. Try again in a little while.');
+          return;
+        }
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setReviews(data);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+      }
+    };
+  
+    fetchReviews();
+  }, []);
+
+  const getCardClass = (index: number) => {
+    const classes = [
+      "review-card1 text-black",
+      "review-card2 text-white",
+      "review-card3",
+      "review-card4",
+      "review-card5 text-white"
+    ];
+    return classes[index % classes.length];
+  };
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+
   return (
     <div className="min-h-screen py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         <div className="">
-          <h2 className="text-4xl text-black font-serif pb-3 ml-12 italic">Reviews:</h2>
+          <h2 className="text-4xl text-black font-serif pb-3 ml-2 italic">Reviews:</h2>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:mr-10 lg:ml-10 md:mr-6 md:ml-6 mr-1 ml-1 lg:grid-cols-4 gap-6">
-          {/* div1 */}
-          <div className="review-card1 transition-colors duration-300 text-black p-8 rounded-lg shadow-lg md:col-span-2">
-            <div className="flex items-center mb-6">
-              <Image
-                className="w-10 h-10 rounded-full mr-4"
-                src="/images/profile.png"
-                alt="First Name, Last Name"
-                width={100}
-                height={100}
-              />
-              <div>
-                <p className="font-semibold">First Name, Last Name</p>
-                <p className="text-gray-700">Author of ...</p>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {reviews.map((review, index) => (
+            <div 
+              key={review.id} 
+              className={`${getCardClass(index)} transition-colors duration-300 p-8 rounded-lg shadow-lg mb-6`}
+            >
+              <div className="flex items-center mb-6">
+                <Image
+                  className="w-10 h-10 rounded-full mr-4"
+                  src="/images/profile.png"
+                  alt={`${review.first_name} ${review.last_name}`}
+                  width={100}
+                  height={100}
+                />
+                <div>
+                  <p className="font-semibold">{`${review.first_name} ${review.last_name}`}</p>
+                  <p className={index === 1 ? "text-gray-200" : index === 4 ? "text-gray-200" : "text-gray-600"}>{review.job_title}</p>
+                </div>
               </div>
+              <p className={index === 1 ? "text-gray-200" : index === 4 ? "text-gray-100" : "text-gray-600"}>
+                {review.review}
+              </p>
             </div>
-            <h4 className="text-xl font-bold mb-4">
-              Lorem ipsum dolor sit amet, erat laboramus efficiantur usu te, sit
-              reque impedit epicuri ne. Eu laudem integre qui. Eu minim nostro
-              nec, modus ullamcorper definitionem cum cu. Ad vel veri illum.
-            </h4>
-            <p className="text-gray-800">
-              Mel te laudem minimum. Mei eros civibus an, vim te quod putant
-              detracto. Ferri regione usu in. Vitae voluptaria ut mel, qui
-              eruditi accommodare ex, pri ea noluisse referrentur. Wisi fuisset
-              qui cu, pri ullum qualisque no. Eum cu mundi iudico definitionem,
-              sed graece deterruisset id, ad duis possit sanctus est.
-            </p>
-          </div>
-
-          {/* div2 */}
-          <div className="review-card2 transition-colors duration-300 text-white p-8 rounded-lg shadow-lg">
-            <div className="flex items-center mb-6">
-              <Image
-                className="w-10 h-10 rounded-full mr-4"
-                src="/images/profile.png"
-                alt="First Name, Last Name"
-                width={100}
-                height={100}
-              />
-              <div>
-                <p className="font-semibold">First Name, Last Name</p>
-                <p className="text-gray-300">Author of ...</p>
-              </div>
-            </div>
-            <h4 className="text-xl font-bold mb-4">
-              Id qui possit legendos constituto, ei sea inermis accumsan.
-            </h4>
-            <p className="text-gray-300">
-              Error petentium patrioque vim an. Ex his quod tota mundi, porro
-              putent everti eum et. Ei usu albucius placerat dissentiet, modus
-              facete nominavi vim ne. Has ut duis minim tamquam, probo primis
-              periculis eu mei.
-            </p>
-          </div>
-
-          {/* div3 */}
-          <div className="review-card3 p-8 transition-colors duration-300 rounded-lg shadow-lg md:row-span-2">
-            <div className="flex items-center mb-6">
-              <Image
-                className="w-10 h-10 rounded-full mr-4"
-                src="/images/profile.png"
-                alt="First Name, Last Name"
-                width={100}
-                height={100}
-              />
-              <div>
-                <p className="font-semibold text-gray-800">
-                  First Name, Last Name
-                </p>
-                <p className="text-gray-600">Author of ...</p>
-              </div>
-            </div>
-            <h4 className="text-xl font-bold mb-4 text-gray-800">
-              Duo diam habemus consetetur cu. Est eu facer delicata.
-            </h4>
-            <p className="text-gray-600">
-              Pri in assum tamquam ancillae, inimicus definitionem cu nec.
-              Albucius definitiones ius ex. Ad decore fabellas molestiae qui.
-              Electram forensibus et sea, his stet illum dicta ex. Summo
-              deseruisse ut vel, quo enim affert atomorum ne. Hinc decore
-              reprehendunt mei no. Ad pro dolor bonorum accumsan, usu putant
-              democritum ea. Legere omittam deterruisset eam id, sit accusamus
-              dignissim cu. Sed et inani offendit, usu adversarium instructior
-              an. Alii graecis rationibus sed ea, at tale perpetua duo. Libris
-              nostrum liberavisse ei cum. Eu vel viderer graecis, vim laudem
-              principes definiebas eu, mel augue principes ut. Vix cu sint veri
-              salutandi, eos ei gubergren sententiae accommodare.
-            </p>
-          </div>
-
-          {/* div4 */}
-          <div className="review-card4 p-8 transition-colors duration-300 rounded-lg shadow-lg">
-            <div className="flex items-center mb-6">
-              <Image
-                className="w-10 h-10 rounded-full mr-4"
-                src="/images/profile.png"
-                alt="First Name, Last Name"
-                width={100}
-                height={100}
-              />
-              <div>
-                <p className="font-semibold text-gray-800">
-                  First Name, Last Name
-                </p>
-                <p className="text-gray-600">Author of ...</p>
-              </div>
-            </div>
-            <h4 className="text-xl font-bold mb-4 text-gray-800">
-              Ad alia ridens qui, duo modo volumus id, ea pro amet modo.
-            </h4>
-            <p className="text-gray-600">
-              QuoAd alia ridens qui, duo modo volumus id, ea pro amet modo. sale
-              eligendi ad, te pro mucius possit dolores, cum no nullam gubergren
-              consequuntur.
-            </p>
-          </div>
-
-          {/* div5 */}
-          <div className="review-card5 transition-colors duration-300 text-white p-8 rounded-lg shadow-lg md:col-span-2">
-            <div className="flex items-center mb-6">
-              <Image
-                className="w-10 h-10 rounded-full mr-4"
-                src="/images/profile.png"
-                alt="First Name, Last Name"
-                width={100}
-                height={100}
-              />
-              <div>
-                <p className="font-semibold">First Name, Last Name</p>
-                <p className="text-gray-200">Author of ...</p>
-              </div>
-            </div>
-            <h4 className="text-xl font-bold mb-4">
-              Ad assum laudem tacimates vel. Habeo cotidieque qui no. Eu cum
-              possit omnesque, omittantur adversarium ius te, per et equidem
-              convenire.
-            </h4>
-            <p className="text-gray-100">
-              Vim cu sale tacimates, oratio nonumes consequat vel cu. Hinc
-              laboramus at pri, sed aperiam persequeris ex, mei ea saepe
-              accusata. Vim autem ignota voluptatibus at, ne sea fugit perpetua,
-              liber affert ut vis. Vis iisque invidunt adversarium eu, cu eos
-              ullum nihil assentior, nibh inani adipiscing id ius.
-            </p>
-          </div>
-        </div>
+          ))}
+        </Masonry>
       </div>
     </div>
   );
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,3 @@
-// src/pages/admin.tsx
-
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -17,7 +15,7 @@ const AdminPage: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
-  const [pendingReviews, setPendingReviews] = useState<Review[]>([]);
+  const [reviews, setReviews] = useState<Review[]>([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState<{ [key: number]: boolean }>({});
   const router = useRouter();
@@ -26,7 +24,7 @@ const AdminPage: React.FC = () => {
     const storedToken = localStorage.getItem("adminToken");
     if (storedToken) {
       setToken(storedToken);
-      fetchPendingReviews(storedToken);
+      fetchAllReviews(storedToken);
     }
   }, []);
 
@@ -48,7 +46,7 @@ const AdminPage: React.FC = () => {
       const data = await response.json();
       setToken(data.token);
       localStorage.setItem("adminToken", data.token);
-      fetchPendingReviews(data.token);
+      fetchAllReviews(data.token);
     } catch (error: unknown) {
       console.error("Login error:", error);
       if (error instanceof Error) {
@@ -61,25 +59,25 @@ const AdminPage: React.FC = () => {
     }
   };
 
-  const fetchPendingReviews = async (authToken: string) => {
+  const fetchAllReviews = async (authToken: string) => {
     try {
       const response = await fetch(
-        "https://159.89.233.75.nip.io/api/admin/pending-reviews",
+        "https://159.89.233.75.nip.io/api/admin/all-reviews",
         {
           headers: { Authorization: `Bearer ${authToken}` },
         }
       );
       if (!response.ok) throw new Error("Failed to fetch reviews");
-      const reviews = await response.json();
-      setPendingReviews(reviews);
+      const allReviews = await response.json();
+      setReviews(allReviews);
     } catch (error) {
-      setError("Failed to fetch pending reviews.");
+      setError("Failed to fetch reviews.");
     }
   };
 
   const handleReviewAction = async (
     reviewId: number,
-    action: "approve" | "reject"
+    action: "approve" | "reject" | "pending"
   ) => {
     setLoading((prev) => ({ ...prev, [reviewId]: true }));
     try {
@@ -95,7 +93,12 @@ const AdminPage: React.FC = () => {
         }
       );
       if (!response.ok) throw new Error(`Failed to ${action} review`);
-      fetchPendingReviews(token);
+      // Update the review status locally
+      setReviews(prevReviews =>
+        prevReviews.map(review =>
+          review.id === reviewId ? { ...review, status: action } : review
+        )
+      );
     } catch (error) {
       console.error(`Failed to ${action} review:`, error);
       setError(`Failed to ${action} review.`);
@@ -110,88 +113,95 @@ const AdminPage: React.FC = () => {
     router.push("/");
   };
 
+  const ReviewSection: React.FC<{ title: string, reviews: Review[], actions: string[] }> = ({ title, reviews, actions }) => (
+    <>
+      <div className="flex flex-row justify-between">
+        <h2 className="text-2xl mb-4 mt-8">{title}</h2>
+        <h2 className="text-2xl mb-4 mt-8">Action: <span className="text-sm">(will move the review to the respective category you pick)</span></h2>
+      </div>
+      {reviews.map((review) => (
+        <div key={review.id} className="bg-white p-4 flex flex-row justify-between rounded shadow-md mb-4">
+          <div>
+            <p><strong>Name:</strong> {review.first_name} {review.last_name}</p>
+            <p><strong>Job Title:</strong> {review.job_title}</p>
+            <p><strong>Review:</strong> {review.review}</p>
+          </div>
+          <div className="ml-10 flex flex-row items-center">
+            {actions.map(action => (
+              <div key={action} className="ml-2">
+                <button
+                  onClick={() => handleReviewAction(review.id, action as "approve" | "reject" | "pending")}
+                  disabled={loading[review.id]}
+                  className={`
+                    ${action === 'approve' ? 'bg-cyan-100 hover:bg-cyan-200' : 
+                      action === 'reject' ? 'bg-orange-100 hover:bg-orange-200' : 
+                      'bg-gray-100 hover:bg-gray-200'}
+                    text-gray-600 hover:text-gray-800 transition-all duration-300 hover:scale-105 p-2 rounded
+                    ${loading[review.id] ? "opacity-50" : ""}
+                  `}
+                >
+                  {loading[review.id] ? "Processing..." : action.charAt(0).toUpperCase() + action.slice(1)}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+
   if (!token) {
     return (
       <div className="min-h-screen bg-purple-50 flex items-center justify-center">
         <form onSubmit={handleLogin} className="bg-white p-8 rounded shadow-md">
-          <h2 className="text-2xl mb-4">Admin Login</h2>
+          <h2 className="text-2xl mb-4 text-purple-900">Admin Login</h2>
           <input
             type="text"
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="w-full p-2 mb-4 border rounded"
+            className="w-full p-2 mb-4 border rounded text-gray-700 focus:border-gray-900 focus:border-1 focus:ring-gray-800 focus:outline-none transition-colors"
           />
           <input
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 mb-4 border rounded"
+            className="w-full p-2 mb-4 border rounded text-gray-700 focus:border-gray-900 focus:border-1 focus:ring-gray-800 focus:outline-none transition-colors"
           />
-          <button type="submit" className="w-full comic-button p-2 rounded">
+          <button type="submit" className="w-full comic-button comic-button-admin p-2 rounded">
             Login
           </button>
-          {error && <p className="text-red-600 mt-2">{error}</p>}
+          {error && <p className="text-red-800 font-serif mt-4 -mb-3">*{error}</p>}
         </form>
       </div>
     );
   }
+
+  const pendingReviews = reviews.filter(review => review.status === 'pending');
+  const approvedReviews = reviews.filter(review => review.status === 'approved' || review.status === 'approve');
+  const rejectedReviews = reviews.filter(review => review.status === 'rejected' || review.status === 'reject');
 
   return (
     <div className="min-h-screen bg-purple-50 p-8">
       <div className="flex flex-row justify-between">
         <h1 className="text-3xl mb-4">Admin Dashboard</h1>
         <div className="w-1/3 md:w-1/6 lg:w-1/12">
-          <button onClick={handleLogout} className="comic-button mb-4">
+          <button onClick={handleLogout} className="comic-button comic-button-admin mb-4">
             Logout
           </button>
         </div>
       </div>
-      <h2 className="text-2xl mb-4">Pending Reviews</h2>
-      {pendingReviews.map((review) => (
-        <div key={review.id} className="bg-white p-4 flex flex-row justify-between rounded shadow-md mb-4">
 
-          <div>
-            <p>
-              <strong>Name:</strong> {review.first_name} {review.last_name}
-            </p>
-            <p>
-              <strong>Job Title:</strong> {review.job_title}
-            </p>
-            <p>
-              <strong>Review:</strong> {review.review}
-            </p>
-          </div>
-
-          <div className="ml-10 flex flex-row items-center">
-            <div className="w-full">
-              <button
-                onClick={() => handleReviewAction(review.id, "approve")}
-                disabled={loading[review.id]}
-                className={`bg-cyan-100 text-gray-500 hover:bg-cyan-200 hover:text-gray-700 transition-all duration-300 hover:scale-105 !p-2 rounded ${
-                  loading[review.id] ? "opacity-50" : ""
-                }`}
-              >
-                {loading[review.id] ? "Processing..." : "Approve"}
-              </button>
-            </div>
-
-            <div className="ml-2 w-full">
-              <button
-                onClick={() => handleReviewAction(review.id, "reject")}
-                disabled={loading[review.id]}
-                className={`bg-orange-100 text-gray-600 hover:bg-orange-200 hover:text-gray-800 transition-all duration-300 hover:scale-105 !p-2 rounded ${
-                  loading[review.id] ? "opacity-50" : ""
-                }`}
-              >
-                {loading[review.id] ? "Processing..." : "Reject"}
-              </button>
-            </div>
-
-          </div>
-        </div>
-      ))}
+      <div className="bg-purple-200 p-8 rounded mb-10">
+        <ReviewSection title="New/Pending Reviews 🌱🪻🌺" reviews={pendingReviews} actions={["approve", "reject"]} />
+      </div>
+      <div className="bg-purple-200 p-8 rounded mb-10">
+        <ReviewSection title="Approved Reviews 🌿🌼🙌 — shows up on your reviews page" reviews={approvedReviews} actions={["pending", "reject"]} />
+      </div>
+      <div className="bg-purple-200 p-8 rounded">
+        <ReviewSection title="Rejected Reviews 😲🙅👎 — will get automatically get deleted every night at 12pm :)" reviews={rejectedReviews} actions={["pending", "approve"]} />
+      </div>
       {error && <p className="text-red-500 mt-2">{error}</p>}
     </div>
   );
