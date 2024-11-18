ceb40ec93ff0d1e81df5802c67b32f000ce0fb97
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-11-17 19:07:29 -0800

    added the amazon button and installed sharp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,24 +27,24 @@ const Home: React.FC = () => {
         {/*         <WisteriaRow />
          */}
 
-      <section className="w-full min-h-screen p-0 m-0 flex flex-col gap-1 xl:-mt-12 lg:flex-row justify-center items-center z-10">
+        <section className="w-full min-h-screen p-0 m-0 flex flex-col gap-1 xl:-mt-12 lg:flex-row justify-center items-center z-10">
           <RightImage className="w-full lg:w-2/3" />
           <LeftImage className="w-full lg:w-1/3" />
 
-      </section>
+        </section>
 
-      <div className="flex items-center relative justify-center z-10 pr-6 pl-6 lg:justify-center lg:flex-row flex-col mt-4 lg:mt-0 w-full space-x-4 gap-6">
-            <ComicButton
-              label="⛰️ See Landscapes →"
-              onClick={() => router.push("/landscape")}
-              className="home-comic-button"
-            />
-            <ComicButton
-              label="Coming Soon"
-              onClick={() => router.push("/coming-soon")}
-              className="home-comic-button"
-            />
-          </div>
+        <div className="flex items-center relative justify-center z-10 pr-6 pl-6 lg:justify-center lg:flex-row flex-col mt-4 lg:mt-0 w-full space-x-4 gap-6">
+          <ComicButton
+            label="⛰️ See Landscapes →"
+            onClick={() => router.push("/landscape")}
+            className="home-comic-button"
+          />
+          <ComicButton
+            label="Buy On Amazon"
+            onClick={() => window.open("https://www.amazon.com/Realm-Unity-V-M-Elyse-ebook/dp/B0DMWG7SNB?crid=1J483U0EEK174&dib=eyJ2IjoiMSJ9._N3krs_EMrGfLMAR3JT9OZYxY0oajre1_KIyICB_js7MzzCGtRY-IRGxZl5lJF6XIWiOd8EDBpWEQXQSr-WbeXjWpxO_ID_Q93iuxwB8QXM7OKyu9UCcNiV9pVInhBzIq1tzDCtg7S3Ufnuvsy1PKUwwh9gHHxGHtn2-ucbpMLnIeiIezRoB5Iud3HVkQnRsDWO2Q4nEeIAq3trXHc4a8g.YI0G1teHbsvO6QnT_fIq7quQ2Fatd5s5L1fRm3aXYx0&dib_tag=se&keywords=the+realm+of+unity&qid=1731820914&sprefix=therealmofunity%2Caps%2C152&sr=8-1#aw-udpv3-customer-reviews_feature_div", "_blank")}
+            className="home-comic-button"
+          />
+        </div>
 
         {/* Butterflies container */}
         {/*         <div className="butterflies-container w-full absolute inset-0 pointer-events-none z-5">
