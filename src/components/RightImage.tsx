3965848fd67f04bf2e316f3ea0df5aadf3ba5c38
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-11-18 10:29:03 -0800

    added added book baby buy button, and added styles to make amazon buy button to appear at top when width < 768px

--- src/components/RightImage.tsx
+++ src/components/RightImage.tsx
@@ -20,55 +20,62 @@ const RightImage: React.FC<RightImageProps> = ({ className }) => {
               </div>
               <br />
 
-              <div className="text-center text-xs">
-                Unity came into being roughly two-hundred years ago, seeded by a
-                small band of humans who had a burning desire for peace and oneness. 
-                With the assistance of the eleventh-dimensional beings, they brought 
-                into form an Earth-like realm and called it Unity.
+              <div className="text-center blurb-text">
+                The Realm of Unity came into being roughly two-hundred years ago. 
+                With the help of extraterrestrials, a small band of humans with 
+                a burning desire for peace and oneness brought into form the Earth-like 
+                realm. They then proceeded to reach out to all the inhabitants of old Earth, 
+                seeking those who were ready, and assisted them with their ascension. 
+                As an increasing amount of light pours onto the Earth, so too grows the 
+                darkness and thus, those-that-seek-to-control become ever more powerful 
+                and diabolical in their destruction of the planet.
                 <br />
                 <br />
 
-                Starting with technologies gifted to them by the extraterrestrials, 
-                they built the first crystal grid in the realm and harnessed the power 
-                of free energy. With unlimited energy at their fingertips, the 
-                firstcomers built a fantastical polycenter. They then proceeded to 
-                reach out to all the inhabitants of old Earth, seeking those who were 
+{/*                 Starting with technologies gifted to them by the extraterrestrials,
+                they built the first crystal grid in the realm and harnessed the power
+                of free energy. With unlimited energy at their fingertips, the
+                firstcomers built a fantastical polycenter. They then proceeded to
+                reach out to all the inhabitants of old Earth, seeking those who were
                 ready to live in oneness, and assisted them with their ascension.
                 <br />
                 <br />
 
-                The entity called Earth is in a powerful ascension process and an 
-                increasing amount of light is pouring onto the planet, slowly 
-                awakening the slumbering masses. As more people wake up to their 
-                divinity, they stream into the utopian realm. As Unity grows, so too 
-                does the power and force of the light that is assisting Earth in her 
-                transition. Old Earth, being a planet of duality, is required to 
-                always have an equal amount of darkness; thus, under the laws of the 
-                universe, those-that-seek-to-control become ever more powerful and 
+                The entity called Earth is in a powerful ascension process and an
+                increasing amount of light is pouring onto the planet, slowly
+                awakening the slumbering masses. As more people wake up to their
+                divinity, they stream into the utopian realm. As Unity grows, so too
+                does the power and force of the light that is assisting Earth in her
+                transition. Old Earth, being a planet of duality, is required to
+                always have an equal amount of darkness; thus, under the laws of the
+                universe, those-that-seek-to-control become ever more powerful and
                 diabolical in their destruction of the planet.
                 <br />
                 <br />
 
-                As the opposing forces intensify, one by one, beings awaken and 
-                collapse duality, creating the possibility of choice. Upon awakening, 
-                they are no longer bound to old Earth and are free to stay and assist 
+                As the opposing forces intensify, one by one, beings awaken and
+                collapse duality, creating the possibility of choice. Upon awakening,
+                they are no longer bound to old Earth and are free to stay and assist
                 humanity or leave for a more pleasant realm.
                 <br />
-                <br />
+                <br /> */}
 
-                <div className="text-sm text-center text-dark-purple-hover">
+                <div className="blurb-text text-center text-dark-purple-hover">
                   🦋 🦋 🦋
                   <br />
                   <br />
-                  Travel with Rose through the enchanting Realm of Unity, 
-                  receive deep wisdom from colorful and quirky Luminoles, and 
-                  discover the true source of power.
+                  On the crumbling dystopia of old Earth, a powerful meditation 
+                  transports Rose to the magical Realm of Unity, a place inhabited 
+                  by colorful and quirky Luminoles. As the secrets of Unity unfold, 
+                  Rose must confront the wounds of her heart to unlock the powers 
+                  hidden within. Can she discover her true calling and find the key 
+                  to saving humanity before it&apos;s too late?
                 </div>
               </div>
 
               <br />
 
-              <div className="text-sm text-center leading-7 text-dark-purple-hover">
+{/*               <div className="text-sm text-center leading-7 text-dark-purple-hover">
                 <span className="italic">
                   Question the nature of reality
                 </span>
@@ -85,7 +92,7 @@ const RightImage: React.FC<RightImageProps> = ({ className }) => {
               <div className="text-lg text-center font-bold text-dark-purple-hover">
                 Take a Transformational Journey from Fear into Love
                 <br />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,20 +28,31 @@ const Home: React.FC = () => {
          */}
 
         <section className="w-full min-h-screen p-0 m-0 flex flex-col gap-1 xl:-mt-12 lg:flex-row justify-center items-center z-10">
+          <div className="pr-16 pl-16 w-full pt-10 pb-2 top-amazon">
+            <ComicButton
+              label="Buy On Amazon"
+              onClick={() => window.open("https://www.amazon.com/Realm-Unity-V-M-Elyse-ebook/dp/B0DMWG7SNB?crid=1J483U0EEK174&dib=eyJ2IjoiMSJ9._N3krs_EMrGfLMAR3JT9OZYxY0oajre1_KIyICB_js7MzzCGtRY-IRGxZl5lJF6XIWiOd8EDBpWEQXQSr-WbeXjWpxO_ID_Q93iuxwB8QXM7OKyu9UCcNiV9pVInhBzIq1tzDCtg7S3Ufnuvsy1PKUwwh9gHHxGHtn2-ucbpMLnIeiIezRoB5Iud3HVkQnRsDWO2Q4nEeIAq3trXHc4a8g.YI0G1teHbsvO6QnT_fIq7quQ2Fatd5s5L1fRm3aXYx0&dib_tag=se&keywords=the+realm+of+unity&qid=1731820914&sprefix=therealmofunity%2Caps%2C152&sr=8-1#aw-udpv3-customer-reviews_feature_div", "_blank")}
+              className="home-comic-button"
+            />
+          </div>
           <RightImage className="w-full lg:w-2/3" />
           <LeftImage className="w-full lg:w-1/3" />
-
         </section>
 
-        <div className="flex items-center relative justify-center z-10 pr-6 pl-6 lg:justify-center lg:flex-row flex-col mt-4 lg:mt-0 w-full space-x-4 gap-6">
+        <div className="flex items-center relative justify-center z-10 pr-6 pl-6 -mt-12 -mb-18 md:mt-16 md:mb-0  lg:justify-center lg:flex-row flex-col lg:mt-0 w-full space-x-4 gap-6">
           <ComicButton
-            label="⛰️ See Landscapes →"
-            onClick={() => router.push("/landscape")}
+            label="Buy On BookBaby"
+            onClick={() => window.open("https://store.bookbaby.com/book/the-realm-of-unity1", "_blank")}
             className="home-comic-button"
           />
           <ComicButton
             label="Buy On Amazon"
             onClick={() => window.open("https://www.amazon.com/Realm-Unity-V-M-Elyse-ebook/dp/B0DMWG7SNB?crid=1J483U0EEK174&dib=eyJ2IjoiMSJ9._N3krs_EMrGfLMAR3JT9OZYxY0oajre1_KIyICB_js7MzzCGtRY-IRGxZl5lJF6XIWiOd8EDBpWEQXQSr-WbeXjWpxO_ID_Q93iuxwB8QXM7OKyu9UCcNiV9pVInhBzIq1tzDCtg7S3Ufnuvsy1PKUwwh9gHHxGHtn2-ucbpMLnIeiIezRoB5Iud3HVkQnRsDWO2Q4nEeIAq3trXHc4a8g.YI0G1teHbsvO6QnT_fIq7quQ2Fatd5s5L1fRm3aXYx0&dib_tag=se&keywords=the+realm+of+unity&qid=1731820914&sprefix=therealmofunity%2Caps%2C152&sr=8-1#aw-udpv3-customer-reviews_feature_div", "_blank")}
+            className="home-comic-button bottom-amazon"
+          />
+            <ComicButton
+            label="⛰️ See Landscapes →"
+            onClick={() => router.push("/landscape")}
             className="home-comic-button"
           />
         </div>
